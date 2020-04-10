03d0680d5e9c538f0360de06f69972c062e63ab1
Alonso Ortiz <alonsoortiz@Alonsos-MacBook-Pro-2.local>
2020-04-10 01:31:40 -0500

    New problem solved.

--- Java/Practice/LostNumbers.java
+++ Java/Practice/LostNumbers.java
@@ -3,11 +3,12 @@
  *
  * Given a set of arrays of scrambled integers, your goal is to find which numbers were lost from each of the sequences.
  * 
- * Numbers are only lost from the middle of a (properly ordered) sequence: the first integer in the sequence l and last integer r are never lost.
- * For this problem, a properly ordered sequence of numbers is to be in strictly increasing order: [l, l + 1, l + 2, ... , r - 2, r - 1, r].
+ * Numbers are only lost from the middle of a (properly ordered) sequence: the first integer in the sequence l and
+ * last integer r are never lost. For this problem, a properly ordered sequence of numbers is to be in strictly 
+ * increasing order: [l, l + 1, l + 2, ... , r - 2, r - 1, r].
  *
- * Your task, for each of the given arrays, is to print a string containing all of the missing numbers from the sequence in increasing order.
- *
+ * Your task, for each of the given arrays, is to print a string containing all of the missing numbers from 
+ * the sequence in increasing order.
  */
 
 import java.io.*;
