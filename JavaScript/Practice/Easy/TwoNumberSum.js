e476cdf68fadf48c1adc2f7bece70cbf6eb56572
Alonso Ortiz <alonsoortiz@Alonsos-MacBook-Pro-3.local>
2021-03-11 22:54:36 -0600

    New problem solved.

---
+++ JavaScript/Practice/Easy/TwoNumberSum.js
@@ -0,0 +1,18 @@
+/*
+ * By: Alonso Ortiz - 11/Mar/2021
+ * From: AlgoExpert
+ */
+
+const twoNumberSum = (arr, target) => {
+    let mySet = new Set();
+
+    for(let el of arr) {
+        if(mySet.has((target-el))) {
+            return [el,(target-el)];
+        } else {
+            mySet.add(el);
+        }
+    }
+
+    return [];
+};
